102ee89a8878903ff3e967ec535237375075389c
Archana <archanamalakhed@gmail.com>
2025-11-07 23:15:45 +0530

    Adding comment for better reading

--- Array-Strings/best_time_sell_stock.py
+++ Array-Strings/best_time_sell_stock.py
@@ -1,16 +1,19 @@
 def best_time_to_buy_stock(prices):
+    #SINGLE TRANSCATION VERSION
     min_price = float('inf')
     max_profit = 0
 
     for price in prices:
-        if price < min_price: # check if this is a new minimum
-            min_price = price
+        if price < min_price: # check if this is a new minimum,  New lowest price → best day to buy
+            min_price = price # always prefer to buy stock at min value so set min value first
         else:
             profit = price - min_price
-            max_profit = max(max_profit,profit)
+            max_profit = max(max_profit,profit) # Potential profit if sold today
         
     return max_profit
 print(best_time_to_buy_stock([7,1,5,3,6,4]))  # Output: 5
 print(best_time_to_buy_stock([7,6,4,3,1]))  
+
+# positive inf is always higher number than any value 7 is less than positive inf first min value will be set to 7
 
 
--- Array-Strings/best_time_sell_stock_on_same_day.py
+++ Array-Strings/best_time_sell_stock_on_same_day.py
@@ -1,9 +1,13 @@
 def maxProfit(prices):
     profit = 0
     for i in range(1, len(prices)):
-        if prices[i] > prices[i - 1]:
+        # if today's price is greater than yesterday's price , sell today
+        if prices[i] > prices[i - 1]: 
             profit += prices[i] - prices[i - 1]
     return profit
 print(maxProfit([7,1,5,3,6,4]))
 print(maxProfit([7,6,4,3,1]))
 print(maxProfit([1,2,3,4,5]))
+
+# You have take stocks for rupee 7 yesterday and today's stock rate is 1 , so we should not sell in order to get profit
+#if you bought stock for 1 rupee and today's rate for that stock is 5 rupee, if you sell stock you get profit . So it is best time to sell stock
