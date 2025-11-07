8e084c59395f26c515b6dcfef68fce306a59ce6a
Archana <archanamalakhed@gmail.com>
2025-11-07 19:30:57 +0530

    Selection Sort

--- Sorting/Selection_sort.py
+++ Sorting/Selection_sort.py
@@ -2,10 +2,12 @@ def selection_sort(arr):
     n = len(arr)
     for i in range(n):
         min_idx = i
-        for j in range(i+1,n):
-            if arr[j]<arr[min_idx]:
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
                 min_idx = j
-                arr[i],arr[min_idx] = arr[min_idx],arr[i]
+        # Swap after inner loop
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
 def print_array(arr):
     for val in arr:
         print(val, end=" ")
@@ -13,12 +15,11 @@ def print_array(arr):
 
 if __name__ == "__main__":
     arr = [64, 25, 12, 22, 11]
-    
+
     print("Original array: ", end="")
     print_array(arr)
-    
+
     selection_sort(arr)
-    
+
     print("Sorted array: ", end="")
     print_array(arr)
-                
